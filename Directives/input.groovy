59e3e1602005d58326633bbb1c53343eea2f6e39
Mohamed AIT LAHCEN <mohamedaitlahcen62@gmail.com>
2025-03-05 08:13:21 +0000

    Updated Directives/input.groovy

--- Directives/input.groovy
+++ Directives/input.groovy
@@ -2,15 +2,15 @@ pipeline{
     // agent section : where to run the pipeline
     agent any
     // input section : contains all the input parameters
-    input {
-        message 'Do you want to proceed?'
-        ok 'Yes'
-        parameters {
-            string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
-        }
-    }
     stages {
         stage('Input Example Stage') {
+            input {
+                    message 'Do you want to proceed?'
+                    ok 'Yes'
+                    parameters {
+                        string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
+                    }
+                }
             steps {
                 echo "Hello, ${params.PERSON}"
             }
