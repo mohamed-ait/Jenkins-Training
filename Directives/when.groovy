714808d6f465ecc465d1f368d40c3736ff4aefae
Mohamed AIT LAHCEN <mohamedaitlahcen62@gmail.com>
2025-03-05 08:22:44 +0000

    feat(jenkins): Add  Directives/when.groovy pipeline

---
+++ Directives/when.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent any
+    environment {
+        DEPLOY_TO = 'production'
+    }
+    stages {
+        stage('Example Build') {
+            steps {
+                echo 'Building the app !'
+            }
+        }
+        stage('Example Deploy') {
+            when {
+               environment name: 'DEPLOY_TO', value: 'production'
+            }
+            steps {
+                echo 'Deploying the app to production !'
+            }
+        }
+    }
+}
